612454a3e53142e669df8e2c01ba643258836ca2
RavinduHasithanjana <ravinduceylofana@outlook.com>
2016-03-07 00:02:26 +0530

    tth

---
+++ HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        String name;
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String _name)
+        {
+            name = _name;
+        }
+
+        public void execution()
+        {
+            String personname;
+            Console.WriteLine("Please enter your name");
+            personname = Console.ReadLine();
+            Person p = new Person();
+            p.setName(personname);
+
+            Console.WriteLine("Hi"+" "+p.getName());
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
